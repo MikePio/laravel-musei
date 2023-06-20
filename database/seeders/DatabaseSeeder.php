3ae8c6c945bd2dae25691c8abaf3de0f62c5a205
Pietromuresu <muresupietro66@gmail.com>
2023-06-20 18:19:04 +0200

    Opera Table + seeder + migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,9 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call(ArtistTableSeeder::class);
+        $this->call([ArtistTableSeeder::class]);
+        $this->call([MuseumTableSeeder::class]);
+        $this->call([OperaTableSeeder::class]);
 
     }
 }
