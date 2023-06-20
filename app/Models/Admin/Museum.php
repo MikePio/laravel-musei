7bdd1fb0f14ed537bb89454d275a10d3c08b7998
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-06-20 16:44:07 +0200

    [add] museum  fillable and resource  controller

--- app/Models/Admin/Museum.php
+++ app/Models/Admin/Museum.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Museum extends Model
 {
     use HasFactory;
+
+    protected $fillable  = [
+        'name',
+        'coordinates',
+        'state',
+        'image',
+        'theme',
+        'price',
+        'ratings',
+        'rooms',
+    ];
 }
